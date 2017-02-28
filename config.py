0c034019909603cd0c6d79d7c28514accef8bf14
simon mackenzie <simon mackenzie>
2017-02-28 19:35:32 +0000

    comments

--- config.py
+++ config.py
@@ -1,23 +1,25 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Feb 18 15:53:27 2017
-
-@author: s
+configuration settings for aws package
 """
 import os
 
+# user for amazon linux AMI
 user = "ec2-user"
+
+# location of keyfile
 keyfile = os.path.join(os.path.expanduser("~"), ".aws", "key.pem")
+
 here = os.path.dirname(os.path.abspath(__file__))
 
 # instance types
 itypes = dict(gpu="p2.xlarge", free="t2.micro")
 
-# cpu=amazon linux
-# gpu=amazon linux with nvidia CUDA 7.5 = "ami-873e61e1"
-amis = dict(gpu="ami-873e61e1", free="ami-c51e3eb6")
+# cpu=amazon linux. gpu=amazon linux with nvidia CUDA 7.5
+# these are in region eu-west-1
+amis = dict(free="ami-c51e3eb6", gpu="ami-873e61e1")
 
-# what is best setting for this????
+# typical spot price 18c/hour. what is best setting for this???
 spotprice = ".25"
 
 # instance specification
@@ -27,14 +29,3 @@ base_spec = dict(ImageId=amis["free"],
                 KeyName="key",
                 MinCount=1, MaxCount=1,
                 BlockDeviceMappings=[])
-
-"""
-note that currently scripts are written for amazon linux
-    centos based
-    user = "ec2_user"
-
-alternative AMIs
-    amazon deep learning = "ami-cb97d5b8"
-    fastai_course_1 "ami-b43d1ec7"
-    amazon linux = "ami-c51e3eb6"
-"""
