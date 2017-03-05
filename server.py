c267ad40a290d709020b53efd0292ba3019b2ac7
simon mackenzie <simon mackenzie>
2017-03-05 11:47:21 +0000

    config simplified. order and comments

--- server.py
+++ server.py
@@ -11,40 +11,37 @@ import apps
 import fabric.api as fab
 from time import sleep
 import pandas as pd
-from config import itypes, amis, spotprice, base_spec, user
+import config as c
 import copy
     
-def create(name, bootsize=None, itype="free", spot=False,
-                            pdrive=None, pdrivesize=10):
-    """ create main types of instance needed for deep learning
+def create(name, itype="free", bootsize=None, pdrive=None, pdrivesize=10,
+                           spot=False):
+    """ create instance and mount pdrive
     
         name = name of instance
-        bootsize = size of boot volume
-            amazon linux AMI default is 8GB
-            docker kaggle/python requires 11GB minimum
         itype = key for itypes dict parameter e.g. free, gpu
+        bootsize = size of boot drive
         pdrive = name of attached, non-boot drive
-        spot = use spot. default is on-demand
+        spot = spot versus on-demand
     """
     if aws.get(name, aws.ec2.instances):
         raise Exception("instance %s already exists"%name)
-    if isinstance(pdrive, str):
-        pdrive = Pdrive(pdrive)
-    spec = copy.deepcopy(base_spec)
+    spec = copy.deepcopy(c.base_spec)
+
+    # instance type
+    spec.update(InstanceType=c.itypes[itype],
+                ImageId=c.useramis[itype])
     
-    # size of root volume
+    # boot drive
     if bootsize:
         bdm = dict(DeviceName="/dev/xvda",
                    Ebs=dict(VolumeType="gp2",
                            VolumeSize=bootsize))
         spec["BlockDeviceMappings"].append(bdm)
-        
-    # instance type
-    spec.update(InstanceType=itypes[itype],
-                ImageId=amis[itype])
-    
-    # if persistent volume then add to instance launch
+            
+    # add pdrive to instance launch
     if pdrive:
+        pdrive = Pdrive(pdrive)
         Ebs=dict(DeleteOnTermination=False,
                  VolumeType="gp2",
                  VolumeSize=pdrivesize)
@@ -55,17 +52,16 @@ def create(name, bootsize=None, itype="free", spot=False,
                    Ebs=Ebs)
         spec["BlockDeviceMappings"].append(bdm)
         
-    # spot or on-demand
+    # create spot or on-demand instance
     if spot:
         instance = create_spot(spec)
     else:
         instance = aws.ec2.create_instances(**spec)[0]
-    
+    aws.set_name(instance, name)
     log.info("waiting for instance running")
     instance.wait_until_running()    
-    aws.set_name(instance, name)
     
-    # wait for ip address
+    # wait for ip address and ssh
     while True:
         if instance.public_ip_address:
             break
@@ -73,16 +69,10 @@ def create(name, bootsize=None, itype="free", spot=False,
         sleep(1)
         instance.load()
     log.info("instance %s running at %s"%(name, instance.public_ip_address))
-    
     fab.env.host_string = instance.public_ip_address
-    fab.env.user = user
+    fab.env.user = c.user
     wait_ssh()
 
-    # install docker
-    apps.install_docker()
-    if itype=="gpu":
-        apps.install_nvidia_docker()
-    
     # prepare pdrive
     if pdrive:
         # set name
@@ -91,55 +81,53 @@ def create(name, bootsize=None, itype="free", spot=False,
                 aws.set_name(aws.ec2.Volume(vol["Ebs"]["VolumeId"]), 
                                             pdrive.name)
                 break
-            
-        # if new volume then format and mount
+        # if new volume then format
         if not latest_snapshot:
             pdrive.formatdisk()
         pdrive.mount()
+
+        # install docker
+        apps.install_docker()
         apps.set_docker_folder("/v1")
-        # restore drivers
-        fab.sudo("cp -r /v1 /var/lib/nvidia-docker")
-        # is this needed too?
-        fab.sudo("cp -r /v1 /run/docker/plugins")
-    else:
-        apps.set_docker_folder()
-        
+        if itype=="gpu":
+            apps.install_nvidia_docker()
+    
+    log.info("instance %s ready at %s"%(name, instance.public_ip_address))
     return instance
  
 def create_spot(spec):
     """ returns a spot instance
-    NOTE to autobid without spotprice then have to use AWS browser console
     """
     del spec["MinCount"]
     del spec["MaxCount"]
     requestId = aws.client.request_spot_instances(
                      DryRun=False,
-                     SpotPrice=spotprice,
+                     SpotPrice=c.spotprice,
                      LaunchSpecification=spec) \
                     ["SpotInstanceRequests"] \
                     [0]['SpotInstanceRequestId']
     log.info("spot request submitted")
     
-    # AWS bug. sometimes gives a requestId but the waiter says it does not exist
+    # wait for spot instance
     while True:
+        # sometimes AWS gives a requestId but waiter says it does not exist
         try:
             aws.client.get_waiter("spot_instance_request_fulfilled") \
                                 .wait(SpotInstanceRequestIds=[requestId])
             break
         except:
+            log.warning("waiting for request id")
             sleep(1)
-
-    # wait until fulfilled
     instanceId = aws.client.describe_spot_instance_requests \
                     (SpotInstanceRequestIds=[requestId]) \
                     ['SpotInstanceRequests'][0] \
                     ['InstanceId']
-    log.info("spot request fulfilled")
+    log.info("spot request fulfilled %s"%instanceId)
     return aws.ec2.Instance(instanceId)
     
 def wait_ssh():
-    """ wait for successfull ssh connection """
-    log.info("waiting for ssh")
+    """ wait for ssh server """
+    log.info("waiting for ssh server")
     while True:
         with fab.quiet():
             try:
@@ -165,22 +153,20 @@ def terminate(instance, save_pdrive=True):
             break
 
     pdrive.unmount()
-
-    # reset name so can be reused
-    aws.set_name(instance, "")
     
     # terminate instance before snapshot as instances are costly
-    # note automatically detached when terminates
     instance.terminate()
+    aws.set_name(instance, "")
     log.info("instance terminated")
-
+    
     if save_pdrive:
         pdrive.create_snapshot()
-    
+    # can still be attached even after instance terminated
+    pdrive.detach()
     pdrive.delete_volume()
      
 def get_tasks(target="python"):
-    """ returns dataframe of tasks running inside docker containers
+    """ returns dataframe of tasks on server running inside docker containers
         where task contains target string
     """
     with fab.quiet():
@@ -188,8 +174,8 @@ def get_tasks(target="python"):
                          "$(docker ps -q)")
         if r.failed:
             return None
-        containers = r.splitlines()
-    containers = [c.lstrip("/") for c in containers]
+    containers = r.splitlines()
+    containers = [container.lstrip("/") for container in containers]
     cout = []
     tout = []
     for container in containers:
