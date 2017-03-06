33154852e7153f742be0e1b7ade80508fecf5437
simon <simonm3@gmail.com>
2017-03-06 18:22:54 +0000

    shorter

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name='xdrive',
-      version='1.0',
+      version='1.1',
       description="Puts programs and data on an external drive rather than "\
                   "on the boot drive. This can then be moved between "\
                   "different types of server including spot instances.",
       url='http://github.com/simonm3/xdrive',
-      author='Simon Mackenzie',
-      author_email='simonm3@gmail.com',
-      license='MIT',
       setup_requires=[ "setuptools_git >= 0.3", ],
       packages=find_packages(),
       install_requires=["simonm3", "fabric3", "pandas", "boto3"])
