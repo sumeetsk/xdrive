2b372d72c2387cc8a79f3fc594d47b87b21c3eac
simon <simonm3@gmail.com>
2017-03-06 18:04:21 +0000

    moved

---
+++ xdrive/aws.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+manage aws resources
+    manage tags
+    list resources used
+    
+NOTE: This is a set of functions not a class
+"""
+import logging as log
+import pandas as pd
+import boto3
+
+### connection #############################################################
+
+ec2 = boto3.resource('ec2')
+client = boto3.client('ec2')
+
+### manage tags ##############################################
+    
+def get_tags(res):
+    """ get tags as a normal dict rather than cryptic boto3 format """
+    tags = res.tags or dict()
+    return {tag["Key"]:tag["Value"] for tag in tags}
+        
+def get_tag(res, key):
+    return get_tags(res).get(key, "")
+
+def set_tag(res, key, value):
+    res.create_tags(Tags=[dict(Key=key, Value=value)])
+    
+def get_name(res):
+    """ Name is used to identify resources as alternative to id """
+    return get_tag(res, "Name")
+    
+def set_name(res, value):
+    """ Name is used to identify resources as alternative to id """
+    set_tag(res, "Name", value)
+
+def get(name=None, collections=None, unique=True):
+    """ get resources by name
+        if unique=True then raise exception if more than one
+        if name=None then gets all resources
+        collections can be collection or list of collections
+        collections=None returns instances, volumes, snapshots
+    """
+    # cleanup inputs
+    if collections is None:
+        collections = [ec2.instances, ec2.volumes, ec2.snapshots]
+    if not isinstance(collections, list):
+        collections = [collections]
+
+    # get
+    reslist = []
+    for collection in collections:
+        try:
+            # snapshots collection includes the worlds snapshots!
+            owned = list(collection.all().filter(OwnerIds=["self"]))
+        except:
+            owned = list(collection.all())
+        reslist.extend([res for res in owned if name is None 
+                               or name == get_name(res)])
+    # cleanup outputs
+    if len(reslist) == 0:
+        return None
+    if unique:
+        if len(reslist) == 1:
+            return reslist[0]
+        raise Exception("More than one resource found:\n%s"%reslist)
+    return reslist
+
+### get all resources ####################################################
+
+def get_instances():
+    """ get dataframe of instances """
+    a=[]
+    for i in ec2.instances.all():
+        a.append([get_name(i), i.instance_id, i.image.image_id,
+                  i.instance_type, i.state["Name"],
+                  i.public_ip_address])
+    return pd.DataFrame(a, columns=["name", "instance_id","image","type",
+                                       "state","ip"])
+    
+def get_ips():
+    """ get list of elastic ips """
+    return [ip["PublicIp"] for ip in client.describe_addresses()["Addresses"]]
---
+++ xdrive/server.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+"""
+create and manage server instances
+
+NOTE: This is a set of functions not a class
+"""
+from .drive import Drive
+from . import apps, aws, config as c
+import logging as log
+
+import fabric.api as fab
+from time import sleep
+import pandas as pd
+import copy
+    
+def create(name, itype="free", bootsize=None, drive=None, drivesize=10,
+                           spot=False):
+    """ create instance and mount drive
+    
+        name = name of instance
+        itype = key for itypes dict parameter e.g. free, gpu
+        bootsize = size of boot drive
+        drive = name of attached, non-boot drive
+        spot = spot versus on-demand
+    """
+    if aws.get(name, aws.ec2.instances):
+        raise Exception("instance %s already exists"%name)
+    spec = copy.deepcopy(c.base_spec)
+
+    # instance type
+    spec.update(InstanceType=c.itypes[itype],
+                ImageId=c.amis[itype])
+    
+    # boot drive
+    if bootsize:
+        bdm = dict(DeviceName="/dev/xvda",
+                   Ebs=dict(VolumeType="gp2",
+                           VolumeSize=bootsize))
+        spec["BlockDeviceMappings"].append(bdm)
+            
+    # add drive to instance launch
+    if drive:
+        drive = Drive(drive)
+        Ebs=dict(DeleteOnTermination=False,
+                 VolumeType="gp2",
+                 VolumeSize=drivesize)
+        latest_snapshot = drive.latest_snapshot()
+        if latest_snapshot:
+            Ebs.update(SnapshotId=latest_snapshot.id)
+        bdm = dict(DeviceName="/dev/xvdf",
+                   Ebs=Ebs)
+        spec["BlockDeviceMappings"].append(bdm)
+        
+    # create spot or on-demand instance
+    if spot:
+        instance = create_spot(spec)
+    else:
+        instance = aws.ec2.create_instances(**spec)[0]
+    aws.set_name(instance, name)
+    log.info("waiting for instance running")
+    instance.wait_until_running()    
+    
+    # wait for ip address and ssh
+    while True:
+        if instance.public_ip_address:
+            break
+        log.info("awaiting IP address")
+        sleep(1)
+        instance.load()
+    log.info("instance %s running at %s"%(name, instance.public_ip_address))
+    fab.env.host_string = instance.public_ip_address
+    fab.env.user = c.user
+    wait_ssh()
+
+    # prepare drive
+    if drive:
+        # set name
+        for vol in instance.block_device_mappings:
+            if vol["DeviceName"] == "/dev/xvdf":
+                aws.set_name(aws.ec2.Volume(vol["Ebs"]["VolumeId"]), 
+                                            drive.name)
+                break
+        # if new volume then format
+        if not latest_snapshot:
+            drive.formatdisk()
+        drive.mount()
+
+        # install docker
+        apps.install_docker()
+        apps.set_docker_folder("/v1")
+        if itype=="gpu":
+            apps.install_nvidia_docker()
+    
+    log.info("instance %s ready at %s"%(name, instance.public_ip_address))
+    return instance
+ 
+def create_spot(spec):
+    """ returns a spot instance
+    """
+    del spec["MinCount"]
+    del spec["MaxCount"]
+    requestId = aws.client.request_spot_instances(
+                     DryRun=False,
+                     SpotPrice=c.spotprice,
+                     LaunchSpecification=spec) \
+                    ["SpotInstanceRequests"] \
+                    [0]['SpotInstanceRequestId']
+    log.info("spot request submitted")
+    
+    # wait for spot instance
+    while True:
+        # sometimes AWS gives a requestId but waiter says it does not exist
+        try:
+            aws.client.get_waiter("spot_instance_request_fulfilled") \
+                                .wait(SpotInstanceRequestIds=[requestId])
+            break
+        except:
+            log.warning("waiting for request id")
+            sleep(1)
+    instanceId = aws.client.describe_spot_instance_requests \
+                    (SpotInstanceRequestIds=[requestId]) \
+                    ['SpotInstanceRequests'][0] \
+                    ['InstanceId']
+    log.info("spot request fulfilled %s"%instanceId)
+    return aws.ec2.Instance(instanceId)
+    
+def wait_ssh():
+    """ wait for ssh server """
+    log.info("waiting for ssh server")
+    while True:
+        with fab.quiet():
+            try:
+                fab.sudo("ls")
+                break
+            except:
+                pass
+        sleep(1)
+    log.info("ssh connected")
+
+def terminate(instance, save_drive=True):
+    """ terminate instance and save drive as snapshot """
+    if isinstance(instance, str):
+        instance = aws.get(instance)
+    
+    apps.stop_docker()
+        
+    # get the drive
+    for bdm in instance.block_device_mappings:
+        if bdm["DeviceName"] == "/dev/xvdf":
+            volume = aws.ec2.Volume(bdm["Ebs"]["VolumeId"])
+            drive = Drive(aws.get_name(volume))
+            break
+
+    drive.unmount()
+    
+    # terminate instance before snapshot as instances are costly
+    instance.terminate()
+    aws.set_name(instance, "")
+    log.info("instance terminated")
+    
+    if save_drive:
+        drive.create_snapshot()
+    # can still be attached even after instance terminated
+    drive.detach()
+    drive.delete_volume()
+     
+def get_tasks(target="python"):
+    """ returns dataframe of tasks on server running inside docker containers
+        where task contains target string
+    """
+    with fab.quiet():
+        r = fab.run("docker inspect --format='{{.Name}}' "\
+                         "$(docker ps -q)")
+        if r.failed:
+            return None
+    containers = r.splitlines()
+    containers = [container.lstrip("/") for container in containers]
+    cout = []
+    tout = []
+    for container in containers:
+        tasks = fab.run("docker exec %s ps -eo args | grep %s || true"\
+                        %(container, target)).splitlines()
+        for task in tasks:
+            cout.append(container)
+            tout.append(task)
+    out = pd.DataFrame(dict(container=cout, task=tout))
+    return out 
