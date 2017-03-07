4716dcc823fd675d47cee35b23883f0c2593fde4
simon <simonm3@gmail.com>
2017-03-07 14:39:15 +0000

    simpler

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
-from setuptools import setup, find_packages
-
+""" minimal setup """
+from setuptools import setup
 setup(name='xdrive',
-      version='1.1.1',
-      description="Puts programs and data on an external drive rather than "\
-                  "on the boot drive. This can then be moved between "\
-                  "different types of server including spot instances.",
+      version='1.1.3',
       url='http://github.com/simonm3/xdrive',
       setup_requires=[ "setuptools_git >= 0.3", ],
-      packages=find_packages(),
+      description="Portable drive that can be moved between AWS instances",
       install_requires=["simonm3", "fabric3", "pandas", "boto3"])
