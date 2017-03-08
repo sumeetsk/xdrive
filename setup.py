55e4cb187e1542673659fc0c4d61fcb48bbdc736
simon <simonm3@gmail.com>
2017-03-08 13:31:49 +0000

    update version

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 """ minimal setup """
 from setuptools import setup
 setup(name='xdrive',
-      version='1.1.3',
+      version='1.2.2',
       url='http://github.com/simonm3/xdrive',
       setup_requires=[ "setuptools_git >= 0.3", ],
       description="Portable drive that can be moved between AWS instances",
-      py_modules=[],
       install_requires=["simonm3", "fabric3", "pandas", "boto3"])
